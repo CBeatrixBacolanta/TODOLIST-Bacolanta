424e0cb0db7c5de30ef80f5d6ce2f3aa74741944
CBeatrix <bacolanta.celeste173@gmail.com>
2025-03-11 00:16:14 +0800

    COMMIT

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'TODOLIST-CBacolanta',  // <-- Replace with your repository name
+  base: '/TODOLIST-CBacolanta',
 });
